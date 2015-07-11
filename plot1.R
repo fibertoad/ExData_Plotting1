b93e5a341d0e5539c8d54f76b1dd113e51e27a57
fibertoad <ryane.kraft@gmail.com>
2015-07-11 15:17:42 -0700

    Delete plot1.R

--- plot1.R
+++
@@ -1,3 +0,0 @@
-hist(power_volts$global_active_power, ylim=c(0,1200), col="red", main= "Global Active Power", xlab= "Global active power (kilowatts)", cex.axis=.8)
-dev.copy(png, file= "Plot 1.png")
-dev.off()
